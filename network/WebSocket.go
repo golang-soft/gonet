af34faa9a20c27a8fcb930c93cca709bb466bb13
jooo000hn@163.com <jooo000hn@163.com>
2021-12-13 18:06:40 +0800

    调整websocket接口

--- network/WebSocket.go
+++ network/WebSocket.go
@@ -2,13 +2,22 @@ package network
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"gonet/rpc"
 	"log"
 	"net/http"
 	"sync"
 	"sync/atomic"
+	//"golang.org/x/net/websocket"
+)
 
-	"golang.org/x/net/websocket"
+var (
+	upgrader = websocket.Upgrader{
+		//允许跨域访问
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
 )
 
 type IWebSocket interface {
@@ -49,7 +58,12 @@ func (this *WebSocket) Start() bool {
 
 	go func() {
 		var strRemote = fmt.Sprintf("%s:%d", this.m_sIP, this.m_nPort)
-		http.Handle("/ws", websocket.Handler(this.wserverRoutine))
+		//var strRemote1 = fmt.Sprintf("%d", this.m_sIP, this.m_nPort)
+		//http.Handle("/ws", websocket.Handler(this.wserverRoutine))
+		//http.Handle("/socket.io/", websocket.Handler(this.wserverRoutine))
+		http.HandleFunc("/socket.io/", this.wserverRoutine2)
+		//http.HandleFunc("/socket.io/", helloHandler)
+
 		err := http.ListenAndServe(strRemote, nil)
 		if err != nil {
 			fmt.Errorf("WebSocket ListenAndServe:%v", err)
@@ -84,7 +98,7 @@ func (this *WebSocket) AddClinet(tcpConn *websocket.Conn, addr string, connectTy
 		pClient.SetMaxPacketLen(this.GetMaxPacketLen())
 		pClient.m_ClientId = this.AssignClientId()
 		pClient.m_sIP = addr
-		pClient.SetConn(tcpConn)
+		pClient.SetConn(tcpConn.UnderlyingConn())
 		pClient.SetConnectType(connectType)
 		this.m_ClientLocker.Lock()
 		this.m_ClientList[pClient.m_ClientId] = pClient
@@ -158,7 +172,7 @@ func (this *WebSocket) handleConn(tcpConn *websocket.Conn, addr string) bool {
 		return false
 	}
 
-	tcpConn.PayloadType = websocket.BinaryFrame
+	//tcpConn.PayloadType = websocket.BinaryFrame
 	pClient := this.AddClinet(tcpConn, addr, this.m_nConnectType)
 	if pClient == nil {
 		return false
@@ -166,4 +180,48 @@ func (this *WebSocket) handleConn(tcpConn *websocket.Conn, addr string) bool {
 
 	pClient.Start()
 	return true
+}
+
+func (this *WebSocket) wserverRoutine2(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("hello"))
+	//收到http请求(upgrade),完成websocket协议转换
+	//在应答的header中放上upgrade:websoket
+	var (
+		conn *websocket.Conn
+		err  error
+		//msgType int
+		//data []byte
+	)
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		//报错了，直接返回底层的websocket链接就会终断掉
+		return
+	}
+	//得到了websocket.Conn长连接的对象，实现数据的收发
+	//for {
+	//	//Text(json), Binary
+	//	//if _, data, err = conn.ReadMessage(); err != nil {
+	//	if _, data, err = conn.ReadMessage(); err != nil {
+	//		//报错关闭websocket
+	//		goto ERR
+	//	}
+	//	//发送数据，判断返回值是否报错
+	//	if err = conn.WriteMessage(websocket.TextMessage, data); err != nil {
+	//		//报错了
+	//		goto ERR
+	//	}
+	//}
+
+	fmt.Printf("客户端：%s已连接！\n", conn.RemoteAddr().String())
+	this.handleConn(conn, conn.RemoteAddr().String())
+
+	//error的标签
+	//ERR:
+	//	conn.Close()
+
+}
+
+func (this *WebSocket) helloHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Write([]byte("Hello World!"))
+	fmt.Printf("客户端：%s已连接！\n", r.RemoteAddr)
+
 }

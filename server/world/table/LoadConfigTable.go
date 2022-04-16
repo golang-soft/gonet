89bcf4c09e5d448001319f24a51bf8e74ed77cbf
jooo000hn <jooo000hn@163.com>
2022-04-16 08:49:20 +0800

    调整log代码

--- server/world/table/LoadConfigTable.go
+++ server/world/table/LoadConfigTable.go
@@ -24,16 +24,23 @@ func Init() {
 	config.InitEnv("local")
 	JsonParse := base.NewJsonTableData()
 
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("BoxTable.json"), BOX_CONFIG)
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("HelpTable.json"), HELP_CONFIG)
 	JsonParse.LoadJsonTableData(config.GetConfigTablePath("HeroAttrTable.json"), RAW_HERO_ATTR)
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("ItemTable.json"), ITEM_CONFIG)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("SkillTable.json"), SKILL_BASIC_INFO)
 	JsonParse.LoadJsonTableData(config.GetConfigTablePath("Map.json"), MAP_CONFIG)
 	JsonParse.LoadJsonTableData(config.GetConfigTablePath("RandNameTable.json"), RAND_NAME_CONFIG)
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("RankRewardTable.json"), RANKREWARD_TABLE_DATA)
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("SkillTable.json"), SKILL_BASIC_INFO)
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("StrengthenTable.json"), STRENGTHEN_TABLE_DATA)
-	JsonParse.LoadJsonTableData(config.GetConfigTablePath("StrTable.json"), STR_TABLE_CONFIG)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("ItemTable.json"), ITEM_CONFIG)
 	JsonParse.LoadJsonTableData(config.GetConfigTablePath("WorldParamTable.json"), WORLD_CONFIG)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("RankRewardTable.json"), RANKREWARD_TABLE_DATA)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("StrengthenTable.json"), STRENGTHEN_TABLE_DATA)
+
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("SuiTable.json"), STRENGTHEN_TABLE_DATA)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("BattlePassRankRewardTable.json"), STRENGTHEN_TABLE_DATA)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("game_server.json"), STRENGTHEN_TABLE_DATA)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("room_server.json"), STRENGTHEN_TABLE_DATA)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("create_new_player.json"), STRENGTHEN_TABLE_DATA)
+
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("BoxTable.json"), BOX_CONFIG)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("HelpTable.json"), HELP_CONFIG)
+	JsonParse.LoadJsonTableData(config.GetConfigTablePath("StrTable.json"), STR_TABLE_CONFIG)
 
 }
